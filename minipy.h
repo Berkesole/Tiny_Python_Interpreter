dbe32cdce7390472fb109fe793643e569d78844c
Berkesole <njruikeoklint@outlook.com>
2019-12-02 00:19:52 +0800

    new file

--- minipy.h
+++ minipy.h
@@ -23,3 +23,35 @@ typedef struct ListElement
     vector<int> place;//元素位置 insert(place.begin(),int)
 }ListElement;
 
+typedef struct cList {
+    TYPE                type;
+    union {
+        int             integer;
+        double          float_number;
+        char*           string_literal;
+        struct cList*   new_List;
+    };
+    struct cList*       next_element;
+} cList;
+
+typedef struct stype {
+    TYPE        type;
+    union {
+        int     iValue;
+        double  dValue;
+        char*   cID;
+        char*   string_literal;
+        cList*  new_List;
+    };
+    ListElement sListElement;
+    char            str;
+} stype;
+
+typedef struct symbol_item {
+    char*               cID;            //标识符
+    struct stype*        slice_TEMP;        //切片
+    struct stype*       stype_items;    //stype指针，指向标识符对应的stype类型变量
+    struct symbol_item* next_element;   //链表
+} symbol_item;
+
+
