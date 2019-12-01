0e3f3481c44cb3a893ee71463bc3cc21dbb492b9
Berkesole <njruikeoklint@outlook.com>
2019-12-02 00:20:21 +0800

    add minipy.com

--- minipy.h
+++ minipy.h
@@ -54,4 +54,33 @@ typedef struct symbol_item {
     struct symbol_item* next_element;   //链表
 } symbol_item;
 
+/*
+ * 返回值的含义：
+ *  0: 类型不符
+ *  1: 运算成功
+ * -1: 被除数为零
+ */
+int stype_Add(stype* lval, stype* rval, stype* result);
+int stype_Minus(stype* lval, stype* rval, stype* result);
+int stype_Mul(stype* lval, stype* rval, stype* result);
+int stype_Div(stype* lval, stype* rval, stype* result);
+int stype_Mod(stype* lval, stype* rval, stype* result);
 
+/* 申请内存请使用这个函数！！！ */
+void* safe_malloc(int size);
+
+void yyerror (char const *s);
+
+void printList (cList* new_List);
+void printAssignExpr(stype* show);
+
+symbol_item* Search_Symbol(char * cID);
+
+void copy_cList(cList* src, cList* dst);
+void copy_stype(stype* src, stype* dst);
+
+void free_stype(stype* target);
+void free_cList(cList* target);
+void free_symbol_item(symbol_item* target);
+
+cList* analysis_ListElement(stype* src);
